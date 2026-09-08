4f8415a80c688b0841e8a0f639ff41c81d9a5a7f
DarkTimes <87787951+C0D6WasTaken@users.noreply.github.com>
2026-09-08 19:26:21 +0300

    Apply suggestion from @ThePedroo
    Co-authored-by: Pedro.js <pedroolimpioguerra@gmail.com>
    Signed-off-by: DarkTimes <87787951+C0D6WasTaken@users.noreply.github.com>

--- src/lib/hiding.c
+++ src/lib/hiding.c
@@ -217,7 +217,7 @@ int do_maps_hiding(struct api_table *api_table, JNIEnv *tw_env) {
     }
     mprotect((void *) map->addr_start, size, map->perms);
 
-    if (map->perms & PROT_EXEC) {
+    if (map->perms & PROT_EXEC)
       __builtin___clear_cache((char*) map->addr_start, (char*) (map->addr_start + size));
     }
   }
